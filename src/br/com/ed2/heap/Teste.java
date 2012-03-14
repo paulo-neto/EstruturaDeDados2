e89078570d9ba86ec7755fb5d3ef187a84cf243e
paulo-neto <paulo.neto@dce.ufpb.br>
2012-03-14 00:25:17 -0300

    No teste modifiquei a exibicao

--- src/br/com/ed2/heap/Teste.java
+++ src/br/com/ed2/heap/Teste.java
@@ -4,42 +4,50 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FilaPrioridade e = new FilaPrioridade(9);
+		FilaPrioridade fp = new FilaPrioridade(9);
+		//{23,17,14,6,13,10,15,7,12}
+		fp.addElemento(23);
+		fp.addElemento(17);
+		fp.addElemento(14);
+		fp.addElemento(6);
+		fp.addElemento(13);
+		fp.addElemento(10);
+		fp.addElemento(15);
+		fp.addElemento(7);
+		fp.addElemento(12);
 		
-		e.addElemento(4);
-		e.addElemento(1);
-		e.addElemento(3);
-		e.addElemento(16);
+		System.out.println("Valores adicionados :");
+		for(int i = 1 ;i <= fp.tamanho_heap;i++)
+			System.out.println(fp.e[i]);
+		System.out.println("\n");
 		
-		
-		for(int i = 1 ;i <= e.tamanho_heap;i++)
-			System.out.println(e.e[i]);
+		System.out.println("Costruindo a Heap :\n");
+		fp.buildMaxHeap();
+		for(int i = 1 ;i <= fp.tamanho_heap;i++)
+			System.out.println(fp.e[i]);
 		
 		System.out.println("\n");
 		
-		e.buildMaxHeap();
-		
-		int j = e.heapExtractMax();
-		
+		System.out.println("Extraindo o maior valor :\n");
+		int j = fp.heapExtractMax();
 		System.out.println(j + "\n");
-		
-		
-		for(int i = 1 ;i <= e.tamanho_heap;i++)
-			System.out.println(e.e[i]);
-		
-		e.heapIncreaseKey(1, 16);;
+		System.out.println("Exibindo a Heap atalizada :\n");
+		for(int i = 1 ;i <= fp.tamanho_heap;i++)
+			System.out.println(fp.e[i]);
 		
 		System.out.println("\n");
 		
-		for(int i = 1 ;i <= e.tamanho_heap;i++)
-			System.out.println(e.e[i]);
-		
-		e.maxHeapInsert(11);
+		System.out.println("Colocando na 2ª posição o valor 16 :\n");
+		fp.heapIncreaseKey(2, 16);;
+		for(int i = 1 ;i <= fp.tamanho_heap;i++)
+			System.out.println(fp.e[i]);
 		
 		System.out.println("\n");
 		
-		for(int i = 1 ;i <= e.tamanho_heap;i++)
-			System.out.println(e.e[i]);
+		System.out.println("Inserindo um novo folha com valor 11 :\n");
+		fp.maxHeapInsert(11);
+		for(int i = 1 ;i <= fp.tamanho_heap;i++)
+			System.out.println(fp.e[i]);
 		
 		
 	}
